0e22ddab9057f120ce153a329ab1b784267db61b
Anna9701 <202340@edu.p.lodz.pl>
2019-10-06 11:04:17 +0200

    Refactor

--- Lab1/files.py
+++ Lab1/files.py
@@ -1,31 +1,28 @@
 import re
 
-def get_anagrams (listOfWords):
-    anagramsDictionary = {}
+def get_anagrams (list_of_words):
+    anagrams_dictionary = {}
 
-    for word in listOfWords:
+    for word in list_of_words:
         characters = sorted(word)
         characters = "".join(characters)
-        if characters in anagramsDictionary:
-            anagramsDictionary[characters].append(word)
+        if characters in anagrams_dictionary:
+            anagrams_dictionary[characters].append(word)
         else:
-            anagramsDictionary[characters] = [word]
+            anagrams_dictionary[characters] = [word]
 
-    return anagramsDictionary
+    return anagrams_dictionary
 
-regex = re.compile('[^a-zA-Z\s]')
-#First parameter is the replacement, second parameter is your input string
+non_letter_regex = re.compile('[^a-zA-Z\s]')
 
 file = open("english.50MB", "r")
-text = file.read()
+text = file.read().lower()
 file.close()
 
-text = text.lower()
-x = regex.sub('', text)
+filtered_text = non_letter_regex.sub('', text)
 
-words = x.split()
+words = filtered_text.split()
 numberOfWords = len(words)
-
 setOfWords = set(words)
 numberOfWordsInSet = len(setOfWords)
 
@@ -37,24 +34,16 @@ print(numberOfWordsInSet)
 print(words[:100])
 
 words = [word for word in words if len(word) > 1]
+print("Number of words with length > 1")
 print(len(words))
 setOfWords = set(words)
+print("Number of unique words with length > 1")
 print(len(setOfWords))
 
-palindroms = [word for word in setOfWords if str(word) == str(word)[::-1]]
-print(len(palindroms))
-print(palindroms[:100])
-
-
-# dictionary = {}
-#
-# for word in setOfWords:
-#     chars = sorted(word)
-#     chars = "".join(chars)
-#     if chars in dictionary:
-#         dictionary[chars].append(word)
-#     else:
-#         dictionary[chars] = [word]
+palindromes = [word for word in setOfWords if str(word) == str(word)[::-1]]
+print("Number of palindromes")
+print(len(palindromes))
+print(palindromes[:100])
 
 dictionary = get_anagrams(setOfWords)
 count = 0
@@ -62,6 +51,7 @@ for key, val in dictionary.items():
     if len(val) > 1:
         count += 1
 
+print("Anagrams count")
 print(count)
 
 ### Exercise 4
@@ -72,23 +62,22 @@ pairsSet = set(pairs)
 print(len(pairsSet))
 
 words = []
-
 for pair in pairsSet:
     words.append("".join(pair))
 
 numberOfWords = len(words)
-
 setOfWords = set(words)
 numberOfWordsInSet = len(setOfWords)
 
-print("Number of words")
+print("Number of pairs")
 print(numberOfWords)
-print("Number of unique words")
+print("Number of unique pairs")
 print(numberOfWordsInSet)
 
-palindroms = [word for word in setOfWords if str(word) == str(word)[::-1]]
-print(len(palindroms))
-print(palindroms[:100])
+palindromes = [word for word in setOfWords if str(word) == str(word)[::-1]]
+print("Number of palindromes")
+print(len(palindromes))
+print(palindromes[:100])
 
 dictionary = get_anagrams(setOfWords)
 count = 0
@@ -96,4 +85,5 @@ for key, val in dictionary.items():
     if len(val) > 1:
         count += 1
 
+print("Number of anagrams")
 print(count)
