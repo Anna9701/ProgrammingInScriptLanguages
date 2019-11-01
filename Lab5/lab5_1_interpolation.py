97ebc8843c2d7b208b5de625aa06219d3f7538cd
Anna9701 <202340@edu.p.lodz.pl>
2019-11-01 17:30:41 +0100

    Interpolation fixes

--- Lab5/lab5_1_interpolation.py
+++ Lab5/lab5_1_interpolation.py
@@ -14,24 +14,6 @@ def ir2(xw, yw, i): #Iloraz roznicowy drugiego stopnia
 def ir3(xw, yw, i): #Iloraz roznicowy trzeciego stopnia
     return (ir2(xw, yw, i+1) - ir2(xw, yw, i)) / (xw[i+3] - xw[i])
 
-x_w = np.array([-1., 0., 1., 2.])
-y_w = np.array([-4., -1., 0., 5.])
-ir_11 = ir1(x_w, y_w, 0)
-ir_12 = ir1(x_w, y_w, 1)
-ir_13 = ir1(x_w, y_w, 2)
-
-ir_21 = ir2(x_w, y_w, 0)
-ir_22 = ir2(x_w, y_w, 1)
-
-ir_31 = ir3(x_w, y_w, 0)
-
-# def ir_k(xw, yw, i, k = 0):
-#     k += 1
-#     x = []
-#     if k + i < len(xw):
-#         x.append((ir_k(xw, yw, i + k) - ir_k(xw, yw, i + k - 1)) / (xw[i + k] - xw[i]))
-#     return x
-
 def i_r(y1, y2, x1, x2):
     return (y2 - y1) / (x2 - x1)
 
@@ -42,11 +24,9 @@ def newton(xw, yw):
         j = n - 1 - k
         tmp = []
         for m in range(j):
-            tmp.append(i_r(tab[k][m], tab[k][m+1], xw[m], xw[m+1]))
+            tmp.append(i_r(tab[k][m], tab[k][m + 1], xw[m], xw[m + k + 1]))
         tab.append(tmp)
     return [t[0] for t in tab]
-
-n = newton(x_w, y_w)
 
 def W(tab, wx, x):
     sum = 0
@@ -68,19 +48,28 @@ def my_inertpld(xw, yw):
         return (x-xw[j]) * (yw[j+1] - yw[j-1]) / (xw[j+1] - xw[j]) + yw[j]
     return f
 
-xp = np.linspace(1, 3)
+a = 1
+b = 3
+xp = np.linspace(a, b)
 fp = my_fun(xp)
 
-y_i2 = [W(n, x_w, i) for i in xp]
-
-x_nodes = np.linspace(1, 3, 5)
+x_nodes = np.linspace(a, b, 5)
 y_nodes = my_fun(x_nodes)
 
-#f_in = interp1d(x_nodes, y_nodes, kind='cubic')
-#y_in = f_in(xp)
+y_numpy_interp = np.interp(xp, x_nodes, y_nodes)
+
+f_scipy_interp = interp1d(x_nodes, y_nodes, kind='cubic')
+y_scipy_interp = f_scipy_interp(xp)
 
 y_i1 = [my_inertpld(x_nodes, y_nodes)]
 
-plt.plot(xp, fp, "g-", x_nodes, y_nodes, "ro", xp, y_i2)
-plt.legend(['function', 'nodes', 'i2'], loc='best')
+x_w = np.array([1., 1.5, 2., 2.5, 3.])
+y_w = my_fun(x_w)
+
+n = newton(x_w, y_w)
+
+y_i2 = [W(n, x_w, i) for i in xp]
+
+plt.plot(xp, fp, "g-", xp, y_scipy_interp, "b.-", xp, y_numpy_interp, "r.:", xp, y_i2, "k.-")
+plt.legend(['function', 'scipy_interpld', 'numpy_interp', 'newton'], loc='best')
 plt.show()
