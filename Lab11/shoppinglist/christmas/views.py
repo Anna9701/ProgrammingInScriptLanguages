68f8562b800c9e0ea477ec7b638e1ab363e99c53
Anna9701 <202340@edu.p.lodz.pl>
2019-12-19 20:44:59 +0100

    Added total for shops and fixed redirection

--- Lab11/shoppinglist/christmas/views.py
+++ Lab11/shoppinglist/christmas/views.py
@@ -1,5 +1,8 @@
+from decimal import Decimal
+
 from django.shortcuts import render, get_object_or_404, redirect
 from django.forms import ModelForm
+from django.template.defaulttags import register
 
 # Create your views here.
 
@@ -7,21 +10,33 @@ from Lab11.shoppinglist.christmas.Record import Record
 from .models import ShoppingRecord, Shop, Product
 
 
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
+
+
 def index(request):
     records = dict()
+    totals = dict()
     for record in ShoppingRecord.objects.all():
         if not (record.shop.name in records):
             records[record.shop.name] = list()
         records[record.shop.name].append(
             Record(record.id, record.product.name, record.amount, record.price(), record.isBought))
-    return render(request, 'list.html', {'records': records})
+    for key, values in records.items():
+        totals[key] = Decimal(0)
+        for record in values:
+            if not record.isBought:
+                totals[key] += Decimal(record.price)
+
+    return render(request, 'list.html', {'records': records, 'totals': totals})
 
 
 def mark_bought(request, record_id):
     record = get_object_or_404(ShoppingRecord, pk=record_id)
     record.isBought = True
     record.save()
-    return index(request)
+    return redirect('/christmas')
 
 
 def add_new(request):
